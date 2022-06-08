6c25041979280ebee802dc20e29bed93aba0c6ff
Felicia Ma <47785408+mabxxyun@users.noreply.github.com>
2022-06-08 14:41:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gui import GUI
+from videoTrack import img_load, video_track
 import argparse
+import cv2
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -8,12 +10,34 @@ def parse_args():
         "-m", 
         type=str,
         choices=["cv2", "mvc", "mesh"],
-        default="mesh",
+        default="cv2",
     )
+    parser.add_argument(
+        "--img_file",
+        type=str,
+        help="Path of the image.",
+        default="./image/dog.jpg",
+    )
+    
+    parser.add_argument(
+        "--video_file",
+        type=str,
+        help="Path of the video.",
+        default="./video/Bear_orig.avi",
+    )
+
+
+    parser.add_argument("-v", "--video", action='store_true')
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = parse_args()
-    loader = GUI(mode=args.mode)
-    loader.master.mainloop()
+    if args.video:
+        img = img_load(args.img_file)
+        
+        video_track(args.video_file)
+    else:
+        loader = GUI(mode=args.mode)
+        loader.master.mainloop()
+
